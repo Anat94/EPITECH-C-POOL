19ea50f70a59fbbc1d2a97b349f1bc5eb83cc3b5
Anat94 <anatole.babin@epitech.eu>
2022-01-16 21:18:27 +0100

    [RENAME & REORGANISZE REPO]

---
+++ my_print_comb2.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2021
+** CPOOL-Day1
+** File description:
+** my_print_comb2.c
+*/
+
+void my_print_comb2()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+
+    while (a <= 9) {
+        while (b <= 9) {
+            while (c <= 9) {
+                while (d <= 9) {
+                    if (a * 10 + b == c * 10 + d) {
+
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    my_print_comb2();
+}
